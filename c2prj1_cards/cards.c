4b14879b2447c16b140365f0ed23c4253ddc3e84
Coursera Learner <nobody@nowhere.nul>
2021-02-07 22:03:05 +0200

    poker assignment part 1

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -4,35 +4,158 @@
 #include "cards.h"
 
 
-void assert_card_valid(card_t c) {
-
-
+void assert_card_valid(card_t c)
+{
+  assert(2 <= c.value && c.value <= 14);
+  assert(SPADES <= c.suit && c.suit < NUM_SUITS);
 }
 
-const char * ranking_to_string(hand_ranking_t r) {
-  return "";
+const char *ranking_to_string(hand_ranking_t r)
+{
+  switch (r)
+  {
+  case STRAIGHT_FLUSH:
+    return "Straight Flush";
+  case FOUR_OF_A_KIND:
+    return "Four of a Kind";
+  case FULL_HOUSE:
+    return "Full House";
+  case FLUSH:
+    return "Flush";
+  case STRAIGHT:
+    return "Straight";
+  case THREE_OF_A_KIND:
+    return "Three of a Kind";
+  case TWO_PAIR:
+    return "Two Pair";
+  case PAIR:
+    return "Pair";
+  case NOTHING:
+    return "Nothing";
+  default: return "";
+  }
 }
 
-char value_letter(card_t c) {
-  return 'x';
+char value_letter(card_t c)
+{
+  char value_letter = '0';
+  if ( c.value >= 2 && c.value <= 9 )
+  {
+    return value_letter + c.value;
+  }
+  if ( c.value > 9 && c.value <= VALUE_ACE )
+  {
+    switch (c.value)
+    {
+      case 10:
+	return value_letter;
+      case VALUE_JACK:
+	return 'J';
+      case VALUE_QUEEN:
+	return 'Q';
+      case VALUE_KING:
+	return 'K';
+      case VALUE_ACE:
+	return 'A';
+      default:
+	return 'x';
+    }
+  }
+  return value_letter;
 }
 
-
-char suit_letter(card_t c) {
-  return 'x';
-  
+char suit_letter(card_t c)
+{
+  switch (c.suit)
+  {
+  case SPADES:
+    return 's';
+  case HEARTS:
+    return 'h';
+  case DIAMONDS:
+    return 'd';
+  case CLUBS:
+    return 'c';
+  default:
+    return 'x';
+  }
 }
 
-void print_card(card_t c) {
-
+void print_card(card_t c)
+{
+  printf("%c", value_letter(c));
+  printf("%c", suit_letter(c));
 }
 
-card_t card_from_letters(char value_let, char suit_let) {
+card_t card_from_letters(char value_let, char suit_let)
+{
   card_t temp;
+  if ( '2' <= value_let && value_let <= '9' )
+    {
+      temp.value = value_let - '0';
+    }
+  else
+    {
+      switch (value_let)
+	{
+	case '0':
+	  temp.value = 10;
+	  break;
+	case 'J':
+	  temp.value = VALUE_JACK;
+	  break;
+	case 'Q':
+	  temp.value = VALUE_QUEEN;
+	  break;
+	case 'K':
+	  temp.value = VALUE_KING;
+	  break;
+	case 'A':
+	  temp.value = VALUE_ACE;
+	  break;
+	}
+    }
+  switch (suit_let)
+    {
+  case 's':
+    temp.suit = SPADES;
+    break;
+  case 'h':
+    temp.suit = HEARTS;
+    break;
+  case 'd':
+    temp.suit = DIAMONDS;
+    break;
+  case 'c':
+    temp.suit = CLUBS;
+    break;
+  default:
+    temp.suit = NUM_SUITS;
+    }
+  assert_card_valid(temp);
   return temp;
 }
 
-card_t card_from_num(unsigned c) {
+card_t card_from_num(unsigned c)
+{
+  assert(52 > c && c >= 0);
   card_t temp;
+  if ( 52 > c && c >= 39 )
+    {
+      temp.suit = CLUBS;
+    }
+  else if ( 39 > c && c >= 26 )
+    {
+       temp.suit = DIAMONDS;
+    }
+  else if ( 26 > c && c >= 13 )
+    {
+       temp.suit = HEARTS;
+    }
+  else 
+    {
+       temp.suit = SPADES;
+    }
+  temp.value = (c % 13) + 2;
   return temp;
 }
--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -1,5 +1,158 @@
+#include <stdio.h>
+#include <string.h>
 #include "cards.h"
+#define TEST(expr) ((expr) ? printf("PASSED\n") : \
+		printf("FAILED: '%s', line: %d\n", #expr, __LINE__))
 
-int main(void) {
+void testSpades();
+void testHearts();
+void testDiamonds();
+void testClubs();
+void testChar_to_card(char suit);
+void testRanking();
 
+int main(void)
+{
+  char suit1 = 's';
+  char suit2 = 'h';
+  char suit3 = 'd';
+  char suit4 = 'c';
+  char suit5 = '5';
+  
+  testSpades();
+  testHearts();
+  testDiamonds();
+  testClubs();
+
+  testRanking();
+  testChar_to_card(suit1);
+  testChar_to_card(suit2);
+  testChar_to_card(suit3);
+  testChar_to_card(suit4);
+  testChar_to_card(suit5);
+
+  
+  return 0;
+}
+
+void testSpades()
+{
+  unsigned i;
+  card_t test;
+
+  for(i = 0; i < 13; ++i)
+    {
+      test = card_from_num(i);
+      print_card(test);
+      printf("\n");
+      TEST(SPADES == test.suit);
+      TEST(((i % 13) + 2) == test.value);
+    }
+}
+
+void testHearts()
+{
+  unsigned i;
+  card_t test1;
+
+  for(i = 13; i < 26; ++i)
+    {
+      test1 = card_from_num(i);
+      print_card(test1);
+      printf("\n");
+      TEST(HEARTS == test1.suit);
+      TEST(((i % 13) + 2) == test1.value);
+    }
+}
+
+void testDiamonds()
+{
+  unsigned i;
+  card_t test2;
+
+  for(i = 26; i < 39; ++i)
+    {
+      test2 = card_from_num(i);
+      print_card(test2);
+      printf("\n");
+      TEST(DIAMONDS == test2.suit);
+      TEST(((i % 13) + 2) == test2.value);
+    }
+}
+
+void testClubs()
+{
+  unsigned i;
+  card_t test3;
+
+  for(i = 39; i < 52; ++i)
+    {
+      test3 = card_from_num(i);
+      print_card(test3);
+      printf("\n");
+      TEST(CLUBS == test3.suit);
+      TEST(((i % 13) + 2) == test3.value);
+    }
+}
+
+void testChar_to_card(char suit)
+{
+  printf("Test Char to Card\nSuit: %c\n", suit);
+  unsigned SUIT;
+  char value;
+  unsigned i;
+  card_t test_card;
+  switch (suit)
+    {
+  case 's':
+    SUIT = SPADES;
+    break;
+  case 'h':
+    SUIT = HEARTS;
+    break;
+  case 'd':
+    SUIT = DIAMONDS;
+    break;
+  case 'c':
+    SUIT = CLUBS;
+    break;
+  default:
+    printf("Invalid suit\n");
+    }
+  for(i = '2'; i <= '9'; ++i)
+    {
+      value = i;
+      test_card = card_from_letters(value, suit);
+      TEST((i - '0') == test_card.value);
+      TEST(SUIT == test_card.suit);
+    }
+  value = '0';
+  test_card = card_from_letters(value, suit);
+  TEST(10 == test_card.value);
+  value = 'J';
+  test_card = card_from_letters(value, suit);
+  TEST(VALUE_JACK == test_card.value);
+  value = 'Q';
+  test_card = card_from_letters(value, suit);
+  TEST(VALUE_QUEEN == test_card.value);
+  value = 'K';
+  test_card = card_from_letters(value, suit);
+  TEST(VALUE_KING == test_card.value);
+  value = 'A';
+  test_card = card_from_letters(value, suit);
+  TEST(VALUE_ACE == test_card.value);
+}
+
+void testRanking()
+{
+  printf("Test Ranking\n");
+  TEST(0 == strcmp(ranking_to_string(STRAIGHT_FLUSH), "Straight Flush"));
+  TEST(0 == strcmp(ranking_to_string(FOUR_OF_A_KIND), "Four of a Kind"));
+  TEST(0 == strcmp(ranking_to_string(FULL_HOUSE),"Full House"));
+  TEST(0 == strcmp(ranking_to_string(FLUSH), "Flush"));
+  TEST(0 == strcmp(ranking_to_string(STRAIGHT), "Straight"));
+  TEST(0 == strcmp(ranking_to_string(THREE_OF_A_KIND), "Three of a Kind"));
+  TEST(0 == strcmp(ranking_to_string(TWO_PAIR), "Two Pair"));
+  TEST(0 == strcmp(ranking_to_string(PAIR), "Pair"));
+  TEST(0 == strcmp(ranking_to_string(NOTHING), "Nothing"));
 }
