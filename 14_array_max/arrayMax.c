c764cd8750d9c37eec2f8b4bc1db0c462475dac6
Coursera Learner <nobody@nowhere.nul>
2021-02-13 04:45:46 +0200

    assignment 14

--- 14_array_max/arrayMax.c
+++ 14_array_max/arrayMax.c
@@ -1,45 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int * arrayMax(int * array, int n) {
-  return NULL;
+int *arrayMax(int *array, int n)
+{
+  int *max_num = &array[0];
+  int i;
+  if ( 0 == n )
+    {
+      return NULL;
+    }
+  for(i = 0; i < n; ++i)
+    {
+      if ( array[i] > *max_num )
+	{
+	  max_num = &array[i];
+	}
+    }
+  return max_num;
 }
 
-void doTest(int * array, int n) {
+void doTest(int *array, int n)
+{
   printf("arrayMax(");
-  if (array == NULL) {
+  if (array == NULL)
+    {
     printf("NULL");
-  }
-  else {
-    printf("{");
-    for (int i =0; i < n; i++) {
-      printf("%d", array[i]);
-      if (i < n -1) {
-	printf(", ");
-      }
     }
+  else
+    {
+    printf("{");
+    for (int i =0; i < n; i++)
+      {
+      printf("%d", array[i]);
+      if (i < n -1)
+	{
+	printf(", ");
+	}
+      }
     printf("}");
-  }
+    }
   printf(", %d) is \n", n);
-  int * p = arrayMax (array, n);
-  if (p == NULL) {
+  int *p = arrayMax(array, n);
+  if (p == NULL)
+    {
     printf("NULL\n");
-  }
+    }
   else {
     printf("%d\n", *p);
   }
 }
 
-int main(void) {
+int main(void)
+{
   int array1[] = { 77, 33, 19, 99, 42, 6, 27, 4};
   int array2[] = { -3, -42, -99, -1000, -999, -88, -77};
   int array3[] = { 425, 59, -3, 77, 0, 36};
 
-  doTest (array1, 8);
-  doTest (array2, 7);
-  doTest (array3, 6);
-  doTest (NULL, 0);
-  doTest (array1, 0);
+  doTest(array1, 8);
+  doTest(array2, 7);
+  doTest(array3, 6);
+  doTest(NULL, 0);
+  doTest(array1, 0);
   
   return EXIT_SUCCESS;
 }
