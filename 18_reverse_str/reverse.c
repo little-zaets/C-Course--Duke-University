f78674a2df1281d972a8c51531b2d43e84e53d23
Coursera Learner <nobody@nowhere.nul>
2021-02-16 01:53:33 +0000

    attempt assignment 18

--- 18_reverse_str/reverse.c
+++ 18_reverse_str/reverse.c
@@ -7,18 +7,20 @@ void reverse(char *str)
 {
   size_t i;
   char temp;
-  size_t size;
-  assert(str);
-  size = strlen(str);
-  for(i = 0; i < (size / 2); ++i)
+  size_t size = strlen(str);
+  if ( NULL != str )
     {
-      temp = str[i];
-      str[i] = str[size - 1 - i];
-      str[size - 1 - i] = temp;
+      for(i = 0; i < (size / 2); ++i)
+	{
+	  temp = str[i];
+	  str[i] = str[size - 1 - i];
+	  str[size - 1 - i] = temp;
+	}
     }
 }
 
-int main(void) {
+int main(void)
+{
   char str0[] = "";
   char str1[] = "123";
   char str2[] = "abcd";
@@ -27,10 +29,10 @@ int main(void) {
   char str5[] = "You can be my wingman anyday!";
   char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be no tomorrow!";
   char * array[] = {str0, str1, str2, str3, str4, str5, str6};
-  for (int i = 0; i < 7; i++) {
+  for (int i = 0; i < 7; i++)
+    {
     reverse(array[i]);
     printf("%s\n", array[i]);
-    //printf("jfkdjfkldsj]n");
-  }
+    }
   return EXIT_SUCCESS;
 }
