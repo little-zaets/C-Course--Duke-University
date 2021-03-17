8dad2f04f9b484843e1de3ed96f587c95383c910
Coursera Learner <nobody@nowhere.nul>
2021-03-17 16:19:16 +0200

    completed 29

--- 29_outname/outname.c
+++ 29_outname/outname.c
@@ -3,6 +3,30 @@
 #include <string.h>
 #include "outname.h"
 
-char * computeOutputFileName(const char * inputName) {
-  //WRITE ME
+char *computeOutputFileName(const char *inputName) 
+{
+	char *outputName = malloc((strlen(inputName) + 7) * sizeof(char *));
+	if ( NULL == outputName )
+	{
+		fprintf(stderr, "Malloc failed");
+		return NULL;
+	}
+	if ( NULL != strstr(inputName, ".txt") )
+	{
+		strcpy(outputName, inputName);
+		strcat(outputName, ".counts");
+	}
+	else
+	{
+		outputName = realloc(outputName, (strlen(inputName) + 11 * sizeof(char *)));
+		if (NULL == outputName)
+		{
+			fprintf(stderr, "Realloc failed");
+			return NULL;
+		}
+		strcpy(outputName, inputName);
+		strcat(outputName, ".txt.counts");
+	}
+	return outputName;
 }
+
--- 29_outname/outname_test.c
+++ 29_outname/outname_test.c
@@ -2,15 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 #define NUM_TESTS 3
-int main(void) {
-  char * testNames[NUM_TESTS] = {"input.txt",
-				 "anotherTestFileName.txt",
-				 "somethingelse"};
-  
-  for (int i = 0; i < NUM_TESTS; i++) {
-    char * outName = computeOutputFileName(testNames[i]);
+
+int main(void)
+{
+  char *testNames[NUM_TESTS] = {"input.txt",
+                                "anotherTestFileName.txt",
+                                "somethingelse"};
+
+  for (int i = 0; i < NUM_TESTS; i++)
+  {
+    char *outName = computeOutputFileName(testNames[i]);
     printf("'%s' => '%s'\n", testNames[i], outName);
     free(outName);
   }
